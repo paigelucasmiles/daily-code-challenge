42b3228ba289444070e368c9f1884fdbb794c32b
Paige Miles <paige@lucas-miles.com>
2021-01-30 15:42:22 -0700

    added and wrote a solution for createPhoneNumber.

--- 6kyu.js
+++ 6kyu.js
@@ -52,6 +52,53 @@ function duplicateCount(text){
 
 const text = 'aabb'
 
-duplicateCount(text)
+// duplicateCount(text)
 
-// console.log(string.toLowerCase().split("").sort().join("").match(/(.)\1+/g))
+
+
+// 6kyu: Create Phone Number
+
+// Write a function that accepts an array of 10 integers (between 0 and 9), 
+// that returns a string of those numbers in the form of a phone number.
+// The returned format must be correct in order to complete this challenge.
+// Don't forget the space after the closing parentheses!
+
+const createPhoneNumber = (tenNumberArray) => {
+    let areaCode = [];
+    let secondArray = [];
+    let thirdArray = [];
+    let i = 0;
+
+    while(i <= 2) {
+        areaCode.push(tenNumberArray[i])
+        i++
+    }
+    while(i >= 3 && i <= 5 ) {
+        secondArray.push(tenNumberArray[i])
+        i++
+    }
+    while(i >= 5 && i < tenNumberArray.length ) {
+        thirdArray.push(tenNumberArray[i])
+        i++
+    }
+    let phoneNumber = ""
+    phoneNumber = phoneNumber.concat("(" + areaCode.join("") + ") " + secondArray.join("") + "-" + thirdArray.join(""))
+    console.log(phoneNumber)
+}
+
+const tenNumberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+createPhoneNumber(tenNumberArray)
+
+// better solution? 
+
+// function createPhoneNumber(numbers){
+//     let format = "(xxx) xxx-xxxx";
+    
+//     for(var i = 0; i < numbers.length; i++)
+//     {
+//         format = format.replace('x', numbers[i]);
+//     }
+    
+//     return format;
+// }
