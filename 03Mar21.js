d2caebb0274dd326826d3228dc9a421a89934b54
Paige Miles <paige@lucas-miles.com>
2021-03-08 10:24:54 -0700

    completed Moving Zeros To The End

--- 03Mar21.js
+++ 03Mar21.js
@@ -115,4 +115,23 @@ function perimeter(n) {
     console.log(arraySum * 4)
 }
 
-perimeter(30)
+// perimeter(30)
+
+// 5kyu: Moving Zeros To The End
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// example:
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+    const newArr = arr.filter(num => num !== 0)
+    const numberOfZeros = arr.length - newArr.length
+
+    for(let i = 0; i < numberOfZeros; i++) {
+        newArr.push(0)
+    }
+    console.log(newArr)
+}
+
+moveZeros([false,1,0,1,2,0,1,3,"a"])
