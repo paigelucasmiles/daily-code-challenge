9e29e7079a86d7d55ee931cc55a91b8225b39622
Paige Miles <paige@lucas-miles.com>
2021-03-25 16:32:06 -0600

    added 1108. Defanging an IP Address problem

--- 03Mar21.js
+++ 03Mar21.js
@@ -278,9 +278,28 @@ let table = {
   'Z': 'Zulu',
 }
 
-function to_nato(words) {
-  return words.split('').filter(c => c !== ' ').map(c => table[c.toUpperCase()] || c).join(' ');
-}
+    function to_nato(words) {
+    return words.split('').filter(c => c !== ' ').map(c => table[c.toUpperCase()] || c).join(' ');
+    }
+
 }
 
 to_nato('If, you can read?')
+
+
+// leetcode 
+// 1108. Defanging an IP Address
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+
+// Example 1:
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+// Example 2:
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+// Constraints:
+// The given address is a valid IPv4 address.
