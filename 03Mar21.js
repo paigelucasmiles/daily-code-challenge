1ccbf8720e852febc4edead645a8d1277509271a
Paige Miles <paige@lucas-miles.com>
2021-03-25 16:45:25 -0600

    1480. Running Sum of 1d Array. Completed

--- 03Mar21.js
+++ 03Mar21.js
@@ -286,6 +286,37 @@ let table = {
 
 to_nato('If, you can read?')
 
+// LeetCode: 1480. Running Sum of 1d Array
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+// Example 2:
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+// Example 3:
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+// Constraints:
+// 1 <= nums.length <= 1000
+// -10^6 <= nums[i] <= 10^6
+
+var runningSum = function(nums) {
+    for(let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i] + nums[i - 1]
+    }
+    return nums
+};
+
 
 // leetcode 
 // 1108. Defanging an IP Address
@@ -314,4 +345,4 @@ var defangIPaddr = function(address) {
     console.log(addressArray.join(''))
 };
 
-defangIPaddr('1.1.1.1')
+// defangIPaddr('1.1.1.1')
