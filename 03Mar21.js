449ee2da882a2bed4d5521ce336a4aac320086d1
Paige Miles <paige@lucas-miles.com>
2021-03-26 15:00:43 -0600

    completed. 771. Jewels and Stones

--- 03Mar21.js
+++ 03Mar21.js
@@ -437,3 +437,61 @@ var kidsWithCandies = function(candies, extraCandies) {
     return candies;
 };
 
+// LeetCode 1470. Shuffle the Array
+
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+// Example 1:
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7] 
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+// Example 2:
+// Input: nums = [1,2,3,4,4,3,2,1], n = 4
+// Output: [1,4,2,3,3,2,4,1]
+
+// Example 3:
+// Input: nums = [1,1,2,2], n = 2
+// Output: [1,2,1,2]
+
+// Constraints:
+// 1 <= n <= 500
+// nums.length == 2n
+// 1 <= nums[i] <= 10^3
+
+var shuffle = function(nums, n) {
+    
+    let resultArray = [];
+        
+    for(let i = 0; i < n; i++) {
+        resultArray.push(nums[i], nums[i + n])
+    }
+    return resultArray;
+};
+
+
+LeetCode: 771. Jewels and Stones
+
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. 
+// You want to know how many of the stones you have are also jewels.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+
+// Example 2:
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+
+// Constraints:
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+var numJewelsInStones = function(jewels, stones) {
+    
+    return stones.split('').filter(letter => jewels.includes(letter)).length;
+    
+};
