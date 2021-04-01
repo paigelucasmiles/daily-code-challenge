57d39f9bf70463055ee57a44baf51d350f3f0b95
Paige Miles <paige@lucas-miles.com>
2021-04-01 11:07:32 -0600

    completed. 1281. Subtract the Product and Sum of Digits of an Integer

--- 03Mar21.js
+++ 03Mar21.js
@@ -573,3 +573,46 @@ var smallerNumbersThanCurrent = function(nums) {
 // Constraints:
 // 0 <= num <= 10^6
 
+var numberOfSteps  = function(num) {
+    let count = 0;
+    reduceNum(num);
+    return count;
+    
+    function reduceNum(num) {
+        if(num === 0) {
+            return;
+        } else if (num % 2 === 0) {
+            num = num / 2
+            count++
+            reduceNum(num)
+        } else if (num % 2 !== 0) {
+            num = num - 1
+            count++
+            reduceNum(num)
+        }
+    }
+};
+
+// LeetCode: 1281. Subtract the Product and Sum of Digits of an Integer
+
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+// Example 1:
+// Input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
+
+// Example 2:
+// Input: n = 4421
+// Output: 21
+// Explanation: 
+// Product of digits = 4 * 4 * 2 * 1 = 32 
+// Sum of digits = 4 + 4 + 2 + 1 = 11 
+// Result = 32 - 11 = 21
+
+
+// Constraints:
+// 1 <= n <= 10^5
