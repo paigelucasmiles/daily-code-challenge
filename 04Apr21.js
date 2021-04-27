1b3b44b89eaa73cec722297c42c61878f22bcf0e
Paige Miles <paige@lucas-miles.com>
2021-04-27 14:23:17 -0600

    added 709. To Lower Case

--- 04Apr21.js
+++ 04Apr21.js
@@ -413,3 +413,19 @@ var arrayStringsAreEqual = function(word1, word2) {
         return false
     }
 };
+
+// LeetCode 709. To Lower Case
+
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+// Example 1:
+// Input: "Hello"
+// Output: "hello"
+
+// Example 2:
+// Input: "here"
+// Output: "here"
+
+// Example 3:
+// Input: "LOVELY"
+// Output: "lovely"
