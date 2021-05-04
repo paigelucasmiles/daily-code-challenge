7a96d0384e7f08a376746305193b6db6ee836bb9
Paige Miles <paige@lucas-miles.com>
2021-05-04 15:40:02 -0600

    solved 1295. Find Numbers with Even Number of Digits

--- 05May.js
+++ 05May.js
@@ -22,3 +22,14 @@
 // Constraints:
 // 1 <= nums.length <= 500
 // 1 <= nums[i] <= 10^5
+
+var findNumbers = function(nums) {
+    let count = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i].toString().length % 2 === 0) {
+            count++
+        }
+    }
+    return count;
+};
